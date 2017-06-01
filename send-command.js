bbc876dc93cf2cba39ad1c5c0ceb9a5ba2174892
Renée Kooi <renee@kooi.me>
2017-06-01 15:39:18 +0200

    comment in send-command.js

--- send-command.js
+++ send-command.js
@@ -1,3 +1,14 @@
+/**
+ * Send click action events to a dzen event server.
+ *
+ *    node send-command.js $PORT $EVENT_NAME
+ *
+ * Used with dzen like
+ *
+ *    ^ca(/usr/bin/node /path/to/send-command.js 43545 some-event)click here^ca()
+ *
+ */
+
 var net = require('net')
 var args = process.argv.slice(2)
 var port = parseInt(args[0], 10)
