dbe5e0ff4ca7fa0bdeb9883bfbcd2068fe2077ba
Renée Kooi <renee@kooi.me>
2017-06-03 15:51:25 +0200

    do not use mock dzen in tests

--- test.js
+++ test.js
@@ -2,16 +2,14 @@ var test = require('tape')
 
 var dzen = function (opts) {
   opts = opts || {}
-  opts.path = require.resolve('./mockdzen')
   return require('.')(opts)
 }
 
 test('.exit()', function (t) {
   t.plan(1)
-  var dz = dzen()
+  var dz = dzen({ spawn: false })
   dz.on('data', (line) => {
     t.equal(line.toString(), '^exit()\n')
-    dz.process.kill()
   })
   dz.exit()
 })
